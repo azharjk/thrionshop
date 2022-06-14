333e817d5fcd949bd617d8870fb913f07f982722
azharjk <azhr.project@gmail.com>
2022-06-14 11:17:29 +0700

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
-const Home: NextPage = () => {
-  return <h1 className="font-bold underline">Thrion</h1>;
+const LandingPage: NextPage = () => {
+  return (
+    <>
+      <header className="px-4 grid place-items-center">
+        <div className="w-100 py-12 flex justify-center">
+          <Image
+            src="/logo.png"
+            width={200}
+            height={200}
+            alt="A Logo of Thrion"
+          />
+        </div>
+        <div className="max-w-md">
+          <p className="text-center font-light text-2xl">
+            This should be a description of what Thrion is
+          </p>
+        </div>
+      </header>
+    </>
+  );
 };
 
-export default Home;
+export default LandingPage;
