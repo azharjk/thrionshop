bff824bd0ccebaff9148804826b3b2429c19abcd
azharjk <azhr.project@gmail.com>
2022-06-14 12:11:55 +0700

    Add mobile view for product list

---
+++ components/ProductCard.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+interface ProductCardProps {
+  src: string;
+  alt: string;
+  name: string;
+  price: string;
+}
+
+const ProductCard: React.FunctionComponent<ProductCardProps> = ({
+  src,
+  alt,
+  name,
+  price,
+}) => {
+  return (
+    <div>
+      <div className="w-full flex justify-center">
+        <div className="w-full px-4 max-w-sm">
+          <Image
+            src={src}
+            width={300}
+            height={300}
+            layout="responsive"
+            priority
+            alt={alt}
+          />
+        </div>
+      </div>
+      <div className="px-4">
+        <h3 className="text-center truncate text-lg mt-4">{name}</h3>
+        <p className="text-center truncate text-sm mt-2">{price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 
+import ProductCard from "../components/ProductCard";
+
 const LandingPage: NextPage = () => {
   return (
     <>
       <header className="px-4 grid place-items-center">
-        <div className="w-100 py-12 flex justify-center">
+        <div className="w-100 py-[90px] flex justify-center">
           <Image
             src="/logo.png"
             width={200}
@@ -19,6 +21,40 @@ const LandingPage: NextPage = () => {
           </p>
         </div>
       </header>
+      <main className="mt-[80px] mb-10">
+        <ul className="grid gap-6">
+          <li>
+            <a href="">
+              <ProductCard
+                src="/product_demo.jpeg"
+                alt="Thrion product"
+                name="Product name 1"
+                price="Rp5.000.000,00"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <ProductCard
+                src="/product_demo.jpeg"
+                alt="Thrion product"
+                name="Product name 2"
+                price="Rp5.000.000,00"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <ProductCard
+                src="/product_demo.jpeg"
+                alt="Thrion product"
+                name="Product name 3"
+                price="Rp5.000.000,00"
+              />
+            </a>
+          </li>
+        </ul>
+      </main>
     </>
   );
 };
