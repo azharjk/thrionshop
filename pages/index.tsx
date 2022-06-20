0f1c6425c26609388df5cac411ca048be2eb8b4f
azharjk <azhr.project@gmail.com>
2022-06-20 10:47:29 +0700

    Add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,11 @@ const LandingPage: NextPage = () => {
         </ul>
       </main>
       <Contact />
+      <footer className="px-4 py-6 text-center bg-black">
+        <a className="text-sm text-white" href="https://www.flaticon.com/free-icons/code" title="code icons">
+          Code icons created by Freepik - Flaticon
+        </a>
+      </footer>
     </>
   );
 };
