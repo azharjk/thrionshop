84bed9d699ccef810888ec330c3a2716e3e3cce8
azharjk <azhr.project@gmail.com>
2022-06-27 20:04:55 +0700

    Use env to set products host and port

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    thrionProductsHost: process.env.THRION_PRODUCTS_HOST,
+    thrionProductsPort: process.env.THRION_PRODUCTS_PORT
+  }
 }
 
 module.exports = nextConfig
--- utils/axios.ts
+++ utils/axios.ts
@@ -1,8 +1,12 @@
+import getConfig from "next/config";
+
 import axios from "axios";
+
+const { publicRuntimeConfig } = getConfig();
 
 export const AxiosInstance = async () => {
   const axiosClient = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: `${publicRuntimeConfig.thrionProductsHost}:${publicRuntimeConfig.thrionProductsPort}/api`,
   });
 
   return axiosClient;
